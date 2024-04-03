6ac53e9ca2d629a32f82ee5c48095e5d4a4a98ae
edgar <edgar.esp.carrion@gmail.com>
2024-04-03 11:31:47 -0500

    login frontend base design

--- pdfgenerator-frontend/src/app/components/login/login.component.ts
+++ pdfgenerator-frontend/src/app/components/login/login.component.ts
@@ -4,25 +4,35 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card'; 
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [MatButtonModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule]
+  imports: [MatButtonModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatInputModule,MatIconModule, CommonModule ]
 })
 
 
 export class LoginComponent {
-   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-   })
 
+   showPassword = false;
+  
+
+      loginForm = new FormGroup({
+         email: new FormControl(''),
+         password: new FormControl(''),
+        })
+   
 
    login(){
       console.log(this.loginForm.value);
+   }
+
+   register(){
+
    }
 }
 
