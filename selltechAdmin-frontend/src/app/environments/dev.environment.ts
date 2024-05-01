7c3f8a5065c9c9c2585e3f092615d8a7ca2ffb10
edgar <edgar.esp.carrion@gmail.com>
2024-05-01 16:46:41 -0500

    production statement

--- selltechAdmin-frontend/src/app/environments/dev.environment.ts
+++ selltechAdmin-frontend/src/app/environments/dev.environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    API_URL: 'http://localhost:3000/api/v1'
+    API_URL: 'http://ec2-52-67-77-120.sa-east-1.compute.amazonaws.com:3000/api/v1'
 }
